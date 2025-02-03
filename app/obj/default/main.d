02922c65b7d6e4c8a0b8e469949ff94c83cff7cb
mustapha Lahmer <159028978+mustaphacris@users.noreply.github.com>
2025-02-03 22:04:57 +0100

    Add files via upload

---
+++ app/obj/default/main.d
@@ -0,0 +1,58 @@
+obj/default/main.o: main.c \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/system.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/linker.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/sys/alt_stdio.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/alt_types.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/io.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/alt_types.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/sys/alt_sys_init.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/drivers/inc/opencores_i2c.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/drivers/inc/opencores_i2c_regs.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/drivers/inc/altera_avalon_pio_regs.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/sys/alt_irq.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/nios2.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/priv/alt_legacy_irq.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/drivers/inc/altera_avalon_timer_regs.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/drivers/inc/altera_avalon_timer.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/sys/alt_dev.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/sys/alt_llist.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/priv/alt_dev_llist.h \
+ C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/sys/alt_warning.h
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/system.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/linker.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/sys/alt_stdio.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/alt_types.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/io.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/alt_types.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/sys/alt_sys_init.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/drivers/inc/opencores_i2c.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/drivers/inc/opencores_i2c_regs.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/drivers/inc/altera_avalon_pio_regs.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/sys/alt_irq.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/nios2.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/priv/alt_legacy_irq.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/drivers/inc/altera_avalon_timer_regs.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/drivers/inc/altera_avalon_timer.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/sys/alt_dev.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/sys/alt_llist.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/priv/alt_dev_llist.h:
+
+C:/Users/mulahmer/Desktop/project_mustapha/software/bsp/HAL/inc/sys/alt_warning.h:
